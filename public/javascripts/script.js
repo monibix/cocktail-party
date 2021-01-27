8a9f6f2d588877e5ee3a81195f6341b8f3ff7c28
monibix <monibixv2@gmail.com>
2021-01-27 14:32:49 +0100

    add see password feature at login/signup views

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,3 +6,12 @@ button.addEventListener('click', () => {
 const form = document.querySelector("#user-profile-form")
     form.style.display = 'inline';
 });
+
+function mostrarContrasena(){
+    let tipo = document.getElementById("password");
+    if(tipo.type == "password"){
+        tipo.type = "text";
+    }else{
+        tipo.type = "password";
+    }
+}
